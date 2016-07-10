eb39309179d8fc5e6382f4e5c1770a5b66135582
Hilarion <nagasawahidenori@nagasawa-eitou-no-macbook-pro.local>
2016-07-10 22:10:41 +0900

    RadiusChangeAnimation creation

--- RadiusChangeAnimation/ViewController.swift
+++ RadiusChangeAnimation/ViewController.swift
@@ -9,15 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let animation = CASpringAnimation(keyPath: "cornerRadius")
 
+    @IBOutlet weak var animationView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // animation start
+        startAnimation()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func startAnimation(){
+        animation.mass = 1.0
+        animation.damping = 10.0
+        animation.stiffness = 240.0
+        
+        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseIn)
+        animation.fromValue = 200
+        animation.toValue = 90
+        
+        animation.removedOnCompletion = false
+        animation.fillMode = kCAFillModeForwards
+        animation.duration = 1.0
+        
+        animation.delegate = self
+        
+        animationView.layer.addAnimation(animation, forKey: "animation")
     }
 
 
